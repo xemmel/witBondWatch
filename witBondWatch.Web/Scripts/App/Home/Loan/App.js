023b25a07dc81a7c3e2e7d5213cfb113065d6321
xemmel <lacour@gmail.com>
2014-10-10 09:53:18 +0200

    Added updateButton on Loan

--- witBondWatch.Web/Scripts/App/Home/Loan/App.js
+++ witBondWatch.Web/Scripts/App/Home/Loan/App.js
@@ -4,16 +4,19 @@ var loanCtrl = app.controller('loanCtrl', function ($scope, $http,$interval) {
   $scope.InputLoan = 2569000;
   $scope.bondName = "2,5%-2047 (u. afdrag):";
   $scope.bond = {};
+  $scope.loaded = false;
   $scope.loanCorrected = $scope.InputLoan;
   $scope.updateScreen = function () {
-
+    $scope.loaded = false;
 
     //console.log("click");
     $http({ method: "GET", url: "/Home/getTheBond", params: { bondName: $scope.bondName } }).
       success(function (data) {
+       
         $scope.bond = data;
         $scope.loanCorrected = ($scope.InputLoan * ($scope.bond.Value / 100));
         $scope.updateTime = Date();
+        $scope.loaded = true;
 
         //console.dir(data);
       }).
